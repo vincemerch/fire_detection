019ed942a1c434a9917cd0204484495d6d909dc0
Jyoti Singh <dh207141@dunnhumby.com>
2021-02-08 20:32:18 +0530

    adding module for gas-detection

---
+++ gas-detection.py
@@ -0,0 +1,117 @@
+"""
+This module is for gas-detection
+"""
+import sys, time
+import math
+from MCP3008 import MCP3008
+
+class MQ():
+
+    ######################### Hardware Related Macros #########################
+    MQ_PIN                       = 0        # define which analog input channel you are going to use (MCP3008)
+    RL_VALUE                     = 5        # define the load resistance on the board, in kilo ohms
+    RO_CLEAN_AIR_FACTOR          = 9.83     # RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+                                            # which is derived from the chart in datasheet
+ 
+    ######################### Software Related Macros #########################
+    CALIBARAION_SAMPLE_TIMES     = 50       # define how many samples you are going to take in the calibration phase
+    CALIBRATION_SAMPLE_INTERVAL  = 500      # define the time interval(in milisecond) between each samples in the
+                                            # cablibration phase
+    READ_SAMPLE_INTERVAL         = 50       # define the time interval(in milisecond) between each samples in
+    READ_SAMPLE_TIMES            = 5        # define how many samples you are going to take in normal operation 
+                                            # normal operation
+ 
+    ######################### Application Related Macros ######################
+    GAS_LPG                      = 0
+    GAS_CO                       = 1
+    GAS_SMOKE                    = 2
+
+    def __init__(self, Ro=10, analogPin=0):
+        self.Ro = Ro
+        self.MQ_PIN = analogPin
+        self.adc = MCP3008()
+        
+        self.LPGCurve = [2.3,0.21,-0.47]    # two points are taken from the curve. 
+                                            # with these two points, a line is formed which is "approximately equivalent"
+                                            # to the original curve. 
+                                            # data format:{ x, y, slope}; point1: (lg200, 0.21), point2: (lg10000, -0.59) 
+        self.COCurve = [2.3,0.72,-0.34]     # two points are taken from the curve. 
+                                            # with these two points, a line is formed which is "approximately equivalent" 
+                                            # to the original curve.
+                                            # data format:[ x, y, slope]; point1: (lg200, 0.72), point2: (lg10000,  0.15)
+        self.SmokeCurve =[2.3,0.53,-0.44]   # two points are taken from the curve. 
+                                            # with these two points, a line is formed which is "approximately equivalent" 
+                                            # to the original curve.
+                                            # data format:[ x, y, slope]; point1: (lg200, 0.53), point2: (lg10000,  -0.22)  
+                
+        print("Calibrating...")
+        self.Ro = self.MQCalibration(self.MQ_PIN)
+        print("Calibration is done...\n")
+        print("Ro=%f kohm" % self.Ro)
+    
+    
+    def MQPercentage(self):
+        val = {}
+        read = self.MQRead(self.MQ_PIN)
+        val["GAS_LPG"]  = self.MQGetGasPercentage(read/self.Ro, self.GAS_LPG)
+        val["CO"]       = self.MQGetGasPercentage(read/self.Ro, self.GAS_CO)
+        val["SMOKE"]    = self.MQGetGasPercentage(read/self.Ro, self.GAS_SMOKE)
+        return val
+        
+    ############################################################################ 
+    def MQResistanceCalculation(self, raw_adc):
+        return float(self.RL_VALUE*(1023.0-raw_adc)/float(raw_adc));
+     
+    ############################################################################ 
+    def MQCalibration(self, mq_pin):
+        val = 0.0
+        for i in range(self.CALIBARAION_SAMPLE_TIMES):          # take multiple samples
+            val += self.MQResistanceCalculation(self.adc.read(mq_pin))
+            time.sleep(self.CALIBRATION_SAMPLE_INTERVAL/1000.0)
+            
+        val = val/self.CALIBARAION_SAMPLE_TIMES                 # calculate the average value
+
+        val = val/self.RO_CLEAN_AIR_FACTOR                      # divided by RO_CLEAN_AIR_FACTOR yields the Ro 
+                                                                # according to the chart in the datasheet 
+
+        return val;
+      
+    ############################################################################ 
+    def MQRead(self, mq_pin):
+        rs = 0.0
+
+        for i in range(self.READ_SAMPLE_TIMES):
+            rs += self.MQResistanceCalculation(self.adc.read(mq_pin))
+            time.sleep(self.READ_SAMPLE_INTERVAL/1000.0)
+
+        rs = rs/self.READ_SAMPLE_TIMES
+
+        return rs
+    ############################################################################ 
+    def MQGetGasPercentage(self, rs_ro_ratio, gas_id):
+        if ( gas_id == self.GAS_LPG ):
+            return self.MQGetPercentage(rs_ro_ratio, self.LPGCurve)
+        elif ( gas_id == self.GAS_CO ):
+            return self.MQGetPercentage(rs_ro_ratio, self.COCurve)
+        elif ( gas_id == self.GAS_SMOKE ):
+            return self.MQGetPercentage(rs_ro_ratio, self.SmokeCurve)
+        return 0
+     
+    ############################################################################ 
+    def MQGetPercentage(self, rs_ro_ratio, pcurve):
+        return (math.pow(10,( ((math.log(rs_ro_ratio)-pcurve[1])/ pcurve[2]) + pcurve[0])))
+
+
+
+
+if __name__ == "__main__":
+
+    mq = MQ();
+    perc = mq.MQPercentage()
+    sys.stdout.write("\r")
+    sys.stdout.write("\033[K")
+    sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+    

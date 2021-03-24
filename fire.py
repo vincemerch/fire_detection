1b0496ed3a5bfda1dc591b10d12d1fd55f58732b
Jyoti-Singh-Spec <60130697+Jyoti-Singh-Spec@users.noreply.github.com>
2021-03-25 00:19:42 +0530

    Update fire.py

--- fire.py
+++ fire.py
@@ -1,80 +1,103 @@
-  
+import imutils
+from imutils import paths
+import face_recognition
+import pickle
 import cv2
-import numpy as np
-import smtplib
-import playsound
-import threading
-
-Alarm_Status = False
-Email_Status = False
-Fire_Reported = 0
-
-def play_alarm_sound_function():
-	while True:
-		playsound.playsound('Alarm_Sound.mp3',True)
-
-def send_mail_function():
-
-    recipientEmail = "Enter_Recipient_Email"
-    recipientEmail = recipientEmail.lower()
-
-
-    print("Sending Email")
-
-    # try:
-    #     server = smtplib.SMTP('smtp.gmail.com', 587)
-    #     server.ehlo()
-    #     server.starttls()
-    #     server.login("Enter_Your_Email (System Email)", 'Enter_Your_Email_Password (System Email')
-    #     server.sendmail('Enter_Your_Email (System Email)', recipientEmail, "Warning A Fire Accident has been reported on ABC Company")
-    #     print("sent to {}".format(recipientEmail))
-    #     server.close()
-    # except Exception as e:
-    # 	print(e)
-
-
-# video = cv2.VideoCapture("video_file") # If you want to use webcam use Index like 0,1.
-video = cv2.VideoCapture(0) # If you want to use webcam use Index like 0,1.
+import os
  
+#get paths of each file in folder named Images
+#Images here contains my data(folders of various persons)
+imagePaths = list(paths.list_images('/home/hp/Desktop/data/'))
+knownEncodings = []
+knownNames = []
+# loop over the image paths
+for (i, imagePath) in enumerate(imagePaths):
+    # extract the person name from the image path
+    name = imagePath.split(os.path.sep)[-2]
+    # load the input image and convert it from BGR (OpenCV ordering)
+    # to dlib ordering (RGB)
+    image = cv2.imread(imagePath)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #Use Face_recognition to locate faces
+    boxes = face_recognition.face_locations(rgb,model='hog')
+    # compute the facial embedding for the face
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    # loop over the encodings
+    for encoding in encodings:
+        knownEncodings.append(encoding)
+        knownNames.append(name)
+#save emcodings along with their names in dictionary data
+data = {"encodings": knownEncodings, "names": knownNames}
+#use pickle to save data into a file for later use
+f = open("face_enc", "wb")
+f.write(pickle.dumps(data))
+f.close()
+
+#find path of xml file containing haarcascade file 
+cascPathface = os.path.dirname(
+ cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
+# load the harcaascade in the cascade classifier
+faceCascade = cv2.CascadeClassifier(cascPathface)
+# load the known faces and embeddings saved in last file
+data = pickle.loads(open('face_enc', "rb").read())
+ 
+print("Streaming started")
+video_capture = cv2.VideoCapture(0)
+# loop over frames from the video file stream
 while True:
-    (grabbed, frame) = video.read()
-    if not grabbed:
-        break
-
-    frame = cv2.resize(frame, (960, 540)) 
+    # grab the frame from the threaded video stream
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
  
-    blur = cv2.GaussianBlur(frame, (21, 21), 0)
-    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    # convert the input frame from BGR to RGB 
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # the facial embeddings for face in input
+    encodings = face_recognition.face_encodings(rgb)
+    names = []
+    # loop over the facial embeddings incase
+    # we have multiple embeddings for multiple fcaes
+    for encoding in encodings:
+       #Compare encodings with encodings in data["encodings"]
+       #Matches contain array with boolean values and True for the embeddings it matches closely
+       #and False for rest
+        matches = face_recognition.compare_faces(data["encodings"],
+         encoding)
+        #set name =inknown if no encoding matches
+        name = "Unknown"
+        # check to see if we have found a match
+        if True in matches:
+            #Find positions at which we get True and store them
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+            # loop over the matched indexes and maintain a count for
+            # each recognized face face
+            for i in matchedIdxs:
+                #Check the names at respective indexes we stored in matchedIdxs
+                name = data["names"][i]
+                #increase count for the name we got
+                counts[name] = counts.get(name, 0) + 1
+            #set name which has highest count
+            name = max(counts, key=counts.get)
  
-    lower = [18, 50, 50]
-    upper = [35, 255, 255]
-    lower = np.array(lower, dtype="uint8")
-    upper = np.array(upper, dtype="uint8")
-
-    mask = cv2.inRange(hsv, lower, upper)
  
-    output = cv2.bitwise_and(frame, hsv, mask=mask)
-    
-    no_red = cv2.countNonZero(mask)
-    
-    if int(no_red) > 15000:
-        Fire_Reported = Fire_Reported + 1
-
-    cv2.imshow("output", output)
-    
-    if Fire_Reported >= 1:
-    
-    	if Alarm_Status == False:
-    		threading.Thread(target=play_alarm_sound_function).start()
-    		Alarm_Status = True
-
-    	if Email_Status == False:
-    		threading.Thread(target=send_mail_function).start()
-    		Email_Status = True
-
-
+        # update the list of names
+        names.append(name)
+        # loop over the recognized faces
+        for ((x, y, w, h), name) in zip(faces, names):
+            # rescale the face coordinates
+            # draw the predicted face name on the image
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+             0.75, (0, 255, 0), 2)
+    cv2.imshow("Frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
- 
+video_capture.release()
 cv2.destroyAllWindows()
-video.release()
+
+
